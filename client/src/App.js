0fe8246e300ef96361e759c9cf35367a40042f8c
Aayush Kumar <r3ptoid1@gmail.com>
2021-04-22 12:58:52 +0530

    1. Created login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from './pages/login/login';
 
 const App = () => {
   return (
     <div>
-      <></>
+      <Login />
     </div>
   );
 };
--- client/src/pages/login/login.jsx
+++ client/src/pages/login/login.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import './login.scss';
+import { Container } from 'react-bootstrap';
 
 const AUTH_URI =
   'https://accounts.spotify.com/authorize?client_id=7e75d7815952445b89788150951776ba&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state';
 const Login = () => {
-  return <></>;
+  return (
+    <Container className=" login-container">
+      <a className="btn btn-success btn-lg login-button" href={AUTH_URI}>
+        Login With Spotify
+      </a>
+    </Container>
+  );
 };
 
 export default Login;
