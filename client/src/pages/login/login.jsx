d8dd26b2f4a36687829d139c12676d8c0bd0780a
Aayush Kumar <r3ptoid1@gmail.com>
2021-04-22 12:03:31 +0530

    Initialised

---
+++ client/src/pages/login/login.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const AUTH_URI =
+  'https://accounts.spotify.com/authorize?client_id=7e75d7815952445b89788150951776ba&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state';
+const Login = () => {
+  return <></>;
+};
+
+export default Login;
